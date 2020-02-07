efec045afe1f58ac008c301338e036cac2339c6e
Christoph Wurst <christoph@winzerhof-wurst.at>
2020-02-07 10:47:59 +0100

    Use external Toast implementation and deprecate the OCP API
    Signed-off-by: Christoph Wurst <christoph@winzerhof-wurst.at>

--- core/src/OCP/index.js
+++ core/src/OCP/index.js
@@ -1,12 +1,10 @@
-/**
- *
- */
+import { showSuccess, showWarning, showError, showInfo, showMessage } from '@nextcloud/dialogs'
+
 import * as AppConfig from './appconfig'
 import * as Comments from './comments'
 import Loader from './loader'
 import { loadState } from '@nextcloud/initial-state'
 import Collaboration from './collaboration'
-import Toast from './toast'
 import * as WhatsNew from './whatsnew'
 
 /** @namespace OCP */
@@ -21,6 +19,30 @@ export default {
 		loadState,
 	},
 	Loader,
-	Toast,
+	/**
+	 * @deprecated 19.0.0 use the `@nextcloud/dialogs` package instead
+	 */
+	Toast: {
+		/**
+		 * @deprecated 19.0.0 use `showSuccess` from the `@nextcloud/dialogs` package instead
+		 */
+		success: showSuccess,
+		/**
+		 * @deprecated 19.0.0 use `showWarning` from the `@nextcloud/dialogs` package instead
+		 */
+		warning: showWarning,
+		/**
+		 * @deprecated 19.0.0 use `showError` from the `@nextcloud/dialogs` package instead
+		 */
+		error: showError,
+		/**
+		 * @deprecated 19.0.0 use `showInfo` from the `@nextcloud/dialogs` package instead
+		 */
+		info: showInfo,
+		/**
+		 * @deprecated 19.0.0 use `showMessage` from the `@nextcloud/dialogs` package instead
+		 */
+		message: showMessage,
+	},
 	WhatsNew,
 }
